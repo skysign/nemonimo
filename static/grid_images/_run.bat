f3cf83e58881183345ce1756c23ba00ce9a59df3
ByeongKeon Kim <skysign [at] gmail.com>
2024-05-12 18:23:45 +0900

    run.bat

---
+++ static/grid_images/_run.bat
@@ -0,0 +1,6 @@
+cd C:\Users\skysign\Documents\work_git\nemonimo
+
+@REM firefox.exe ./src/routes/grid.html &
+"C:\Program Files\Mozilla Firefox\firefox.exe" ./src/routes/grid.html &
+
+npm run dev &
